7ccd9c9c58bab12fc2e4f4b7498a009ed43a034f
unknown <stp40@case.edu>
2022-10-07 15:36:38 -0400

    initial commit

---
+++ src/main/java/com/cwrudatingwebsite/cwrudating/CwrudatingApplication.java
@@ -0,0 +1,13 @@
+package com.cwrudatingwebsite.cwrudating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwrudatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwrudatingApplication.class, args);
+	}
+
+}
