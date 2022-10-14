9a3c57131d5fa28241e6b6b39d9623398af8cc32
lsn19 <lsn19@case.edu>
2022-10-14 18:54:33 -0400

    Table Database edit

--- src/main/java/com/cwrudatingwebsite/TableDatabase.java
+++ src/main/java/com/cwrudatingwebsite/TableDatabase.java
@@ -2,8 +2,24 @@ package com.cwrudatingwebsite;
 
 public class TableDatabase {
 
+
+    // Insert, delete, edit and read methods for the Account database
+    // Will need to make more for question classes
     public void insert(Account account, String tablename) {
         //insert into sql
     }
     
+    public boolean delete(Account account, String tablename) {
+        return false;
+    }
+
+    public void edit(Account account, String password, String tablename) {
+        //edit sql
+    }
+
+    public Object read(Account account, String tablename) {
+        Object test = new Object();
+        return test;
+    }
+
 }
