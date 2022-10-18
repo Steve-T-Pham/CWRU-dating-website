ada5ab2b50dbfee8c8aef2f5cee11c33554c83c2
lsn19 <lsn19@case.edu>
2022-10-18 01:17:45 -0400

    Added new class Questionnaire and updated Table Database

---
+++ src/main/java/com/cwrudatingwebsite/Questionnaire.java
@@ -0,0 +1,13 @@
+package com.cwrudatingwebsite;
+
+public class Questionnaire {
+
+    public Questionnaire getQuestionnairePersonal(String caseID) {
+
+    }
+
+    public Questionnaire getQuestionnairePreference(String caseID) {
+
+    }
+    
+}
