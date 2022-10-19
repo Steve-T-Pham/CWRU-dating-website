97c710ee2b6e83ada0f256d8f9647d10e2fa65b2
suchi <srd137@case.edu>
2022-10-19 17:08:50 -0400

    init

---
+++ src/test/java/com/cwrudatingwebsite/QuestionTest.java
@@ -0,0 +1,38 @@
+package com.cwrudatingwebsite;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.beans.Transient;
+
+import org.junit.*;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+
+public class QuestionTest {
+
+    static Question aQuestion = new Question(null, null):
+
+    public static void setupClass(){
+        aQuestion.setQuestion("What is your major");
+        aQuestion.setOptions(null);
+        aQuestion.setAnswer("1");
+    }
+
+    
+    public  void testGetQuestion(){
+        //Expected variable , actual variable , compare
+
+         String expected = "What is your major";
+         String actual = aQuestion.getQuestion();
+    
+        assertEquals("Testing Get Question", expected,actual);
+    }
+
+    
+
+ 
+
+    
+
+
+
+}
