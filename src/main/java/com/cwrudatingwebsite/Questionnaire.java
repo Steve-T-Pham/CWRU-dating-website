b8f0b4a29cb54c58924948638d54b9547872a335
Wendy Wu <96844488+ww-classy-lady@users.noreply.github.com>
2022-10-18 21:38:31 -0400

    adding questionnaire and question classes

--- src/main/java/com/cwrudatingwebsite/Questionnaire.java
+++ src/main/java/com/cwrudatingwebsite/Questionnaire.java
@@ -1,5 +1,17 @@
 package com.cwrudatingwebsite;
 
-public class Questionnaire {
-    
+public class Questionnaire { //all the question objects in this questionnaire
+    private Question[] a; 
+    public Questionnaire(Question[] b)
+    {
+        a=b;
+    }
+    public Question[] getQuestionnaire(){
+        //get all the question objects (with answers) from the database
+        return a;
+    }
+    public void setQuestionnaire(Question[] c){
+        //get all the question objects (with answers) from the database
+        a=c;
+    }
 }
