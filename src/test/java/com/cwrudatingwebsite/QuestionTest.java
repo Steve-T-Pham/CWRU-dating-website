fef9e96da06b4e3c5d80dca7a7ea524d161d3698
Wendy Wu <96844488+ww-classy-lady@users.noreply.github.com>
2022-11-11 21:04:29 -0500

    Revert "init"
    This reverts commit bd7c30cf52740eb9fd5790207ee2ca55c8b80b4d.

--- src/test/java/com/cwrudatingwebsite/QuestionTest.java
+++ src/test/java/com/cwrudatingwebsite/QuestionTest.java
@@ -1,15 +1,15 @@
 package com.cwrudatingwebsite;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.beans.Transient;
 
-import org.junit.*;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.junit.jupiter.api.Test;
+
+import com.cwrudatingwebsite.Question;
 
 public class QuestionTest {
 
-    static Question aQuestion = new Question(null, null);
+    static Question aQuestion = new Question("What is your major", null);
 
     public static void setupClass(){
         aQuestion.setQuestion("What is your major");
@@ -17,20 +17,13 @@ public class QuestionTest {
         aQuestion.setAnswer("1");
     }
 
-    
+    @Test
     public  void testGetQuestion(){
         //Expected variable , actual variable , compare
 
          String expected = "What is your major";
          String actual = aQuestion.getQuestion();
-    
-        assertEquals("Testing Get Question", expected,actual);
-    }
-
-    
-    public void testGetOptions(){
-
-        String expected = "";
+        assertEquals(expected,actual);
     }
 
  
