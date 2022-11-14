9565233f9b09cae2bfd9f1ac12dccf6ddf395289
Wendy Wu <96844488+ww-classy-lady@users.noreply.github.com>
2022-11-13 21:45:14 -0500

    added QuestionATest and other changes.

--- src/test/java/com/cwrudatingwebsite/QuestionTestA.java
+++ src/test/java/com/cwrudatingwebsite/QuestionTestA.java
@@ -3,16 +3,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class QuestionTestA {
- 
-    Question hello;
+class QuestionTestA {
+
     @Test
-    public void getQuestionTest()
+    void getQuestionTest()
     {
-        hello = new Question("What is your Major", null);
+        Question hello = new Question("What is your Major?", null);
         String expect = "What is your Major?";
         String actual = hello.getQuestion();
         assertEquals(expect,actual);
 
+    }
+    @Test
+    void getOptionsTest()
+    {
+        String [] choice = {"pop", "jazz", "classical"};
+        Question hi = new Question("What is your favorite music genre?",choice);
+        String [] real = hi.getOptions();
+        assertEquals(choice,real);
+    }
+    @Test
+    void getAnswerTest()
+    {
+        String [] there = {"pop", "jazz", "classical"};
+        Question hey = new Question("What is your favorite music genre?",there);
+        hey.setAnswer("jazz");
+        assertEquals("jazz",hey.getAnswer());
     }
 }
