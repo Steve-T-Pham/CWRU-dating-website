931a6315826182fb108dc45502246c38061b9990
Wendy Wu <96844488+ww-classy-lady@users.noreply.github.com>
2022-12-07 17:06:40 -0500

    insert classes

---
+++ target/classes/templates/accountInsert.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+ 
+<head>
+    <title>Insert Page page</title>
+</head>
+ 
+<body>
+    <center>
+        <?php
+ 
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => staff
+        $conn = mysqli_connect("localhost", "root", "", "CWRU Dating Website");
+         
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. "
+                . mysqli_connect_error());
+        }
+         
+        // Taking all 5 values from the form data(input)
+        $firstname =  $_REQUEST['firstName'];
+        $lastname = $_REQUEST['lastName'];
+        $username =  $_REQUEST['username'];
+        $password = $_REQUEST['password'];
+        $email = $_REQUEST['email'];
+         
+        // Performing insert query execution
+        // here our table name is college
+        $sql = "INSERT INTO Account VALUES ('$username','$password', '$email', '$firstname', '$lastname')";
+         
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully."
+                . " Please browse your localhost php my admin"
+                . " to view the updated data</h3>";
+ 
+            echo nl2br("\n$firstname\n $lastname\n "
+                . "$username\n $password\n $email");
+        } else{
+            echo "ERROR: Hush! Sorry $sql. "
+                . mysqli_error($conn);
+        }
+         
+        // Close connection
+        mysqli_close($conn);
+        ?>
+    </center>
+</body>
+ 
+</html>
