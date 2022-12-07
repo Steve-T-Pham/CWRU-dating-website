750ae800de13fb25bb8a892323e3eef57f3b7548
Steve-T-Pham <stp40@case.edu>
2022-12-07 13:43:16 -0500

    cleaned up ux and page routing

--- src/main/java/com/cwrudatingwebsite/CustomAccountDetailsService.java
+++ src/main/java/com/cwrudatingwebsite/CustomAccountDetailsService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomAccountDetailsService implements UserDetailsService{
+    
     @Autowired
     private AccountRepository accountRepo;
 
